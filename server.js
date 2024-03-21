a633de5bfc0e087d2bcb068f2bbf7ee7979cc360
ngochuy268 <huyduclong2642000@gmail.com>
2024-03-21 09:41:04 +0700

    fix

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8081;
@@ -28,7 +30,7 @@ connection.connect(error => {
 
 
 // API endpoint để lấy dữ liệu từ cơ sở dữ liệu
-app.get('/pop_music', (req, res) => {
+app.get('/api/pop_music', (req, res) => {
   const query = 'SELECT * FROM pop_music';
   connection.query(query, (error, results) => {
     if (error) {
@@ -43,7 +45,7 @@ app.get('/pop_music', (req, res) => {
 });
 
 // API endpoint để thêm dữ liệu vào cơ sở dữ liệu
-app.post('/pop_music', (req, res) => {
+app.post('/api/pop_music', (req, res) => {
   const {name, content, link_img, link_video, author } = req.body;
   const query = 'INSERT INTO pop_music (name, content, link_img, link_video, author) VALUES (?, ?, ?, ?, ?)';
   connection.query(query, [name, content, link_img, link_video, author], (error, result) => {
@@ -58,26 +60,66 @@ app.post('/pop_music', (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+
+
+// Gửi email
+app.post('/api/send-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Tạo một bộ gửi thư
+  let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'popvibes.net@gmail.com', // Gmail của bạn
+          pass: 'gejl hpdl ergo qsdx' // Mật khẩu Gmail của bạn
+      }
+  });
+
+  // Định nghĩa nội dung email dựa trên thông tin từ biểu mẫu
+  let mailOptions = {
+      from: email, // Địa chỉ email người gửi (được nhập từ biểu mẫu)
+      to: 'popvibes.net@gmail.com', // Địa chỉ email người nhận (địa chỉ cố định)
+      subject: subject, // Tiêu đề email (được nhập từ biểu mẫu)
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}` // Nội dung email (được nhập từ biểu mẫu)
+  };
+
+  // Gửi email
+  transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+          console.log(error);
+          res.status(500).send('Error sending email');
+      } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send('Email sent successfully');
+      }
+  });
 });
-
-
-
-
 
 
 // server {
 //   listen 80;
-//   listen [::]:80;
-//   server_name popvibes.net www.popvibes.net;
+//   server_name popvibes.net;
 
-//   root /var/www/popvibes.net/html;
+//   # Proxy các yêu cầu API đến ứng dụng Node.js chạy trên cổng 8081
+//   location /api/ {
+//       proxy_pass http://localhost:8081;
+//       proxy_http_version 1.1;
+//       proxy_set_header Upgrade $http_upgrade;
+//       proxy_set_header Connection 'upgrade';
+//       proxy_set_header Host $host;
+//       proxy_cache_bypass $http_upgrade;
+//   }
 
-//   index index.html index.htm index.nginx-debian.html;
-
+//   # Phục vụ các tệp tĩnh và index.html từ thư mục build của ứng dụng React
 //   location / {
-//       try_files $uri $uri/ =404;
+//       root /path/to/your/react/app/build;
+//       index index.html index.htm;
+//       try_files $uri $uri/ /index.html;
 //   }
 // }
 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
