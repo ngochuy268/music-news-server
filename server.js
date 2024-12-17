1870872019288a759f830a4a8131cb77fe7140c8
ngochuy268 <huyduclong2642000@gmail.com>
2024-12-17 18:25:56 +0700

    first

--- server.js
+++ server.js
@@ -109,7 +109,10 @@ const emailRoutes = require('./routes/emailRoutes');
 const app = express();
 const port = 8081;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ 
+  origin: ['http://localhost:3000','https://music-news-client.vercel.app'], 
+  methods: 'GET, POST', 
+  allowedHeaders: 'Content-Type, Authorization',  }));
 app.use(express.json());
 
 // Routes
